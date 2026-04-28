10972ad7e6f333d03fbd066d1d101a047a9f2bae
BEOBMA <dingxiao19@gmail.com>
2026-04-28 14:45:28 +0900

    feat: 로그 제거

--- src/main/java/org/beobma/prccore/command/StartTimeFlowCommand.kt
+++ src/main/java/org/beobma/prccore/command/StartTimeFlowCommand.kt
@@ -10,14 +10,11 @@ import org.bukkit.command.CommandSender
 class StartTimeFlowCommand : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (hasStartedTimeFlow()) {
-            sender.sendMessage("[PrcCore] 시간은 이미 흐르고 있습니다.")
             return true
         }
 
         startTimeFlow()
         saveAll()
-
-        sender.server.broadcastMessage("[PrcCore] 튜토리얼 단계가 종료되어 시간이 흐르기 시작합니다.")
 
         return true
     }
